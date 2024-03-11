8eeb8b7b3f017b6afc2000c131c530f325314964
root <root@2476548-it41385.twc1.net>
2024-03-11 13:01:42 +0300

    tests changed

--- Test.cpp
+++ Test.cpp
@@ -4,10 +4,13 @@ extern "C" int MyPrintf(const char* format, ...);
 
 int main()
 {
-    MyPrintf("Ded, aka %s\n"
+    //MyPrintf("%d\n", -0);
+    int len = MyPrintf("Ded, aka %s\n"
              "age: %o / %x / %d / %b\n"
              "favourite char:%c,\n"
-             "random stuff:%d, %c, percents: %%%%%%\n" 
-             "BRB: %%%b + jm r1\n",
-             "LOX", 0116, 0x4E, 78, 0b1001110, 'Z', 56, 'O', 0b1101111);
+             "random stuff: %d, %c, percents: %%%%%%\n" 
+             "BRB: %% + jm r1\n",
+             "LOX", -0116, -78, -78, -(0), 'Z', 0, '\n');
+
+    MyPrintf("len - %d\n", len);
 }
